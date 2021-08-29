42efbb81ff90587398ee41647acf7419aa5af56a
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2021-08-29 13:40:34 -0700

    Add files via upload
    runner_sim.py for 10 laps

---
+++ runner_sim_rcai10.py
@@ -0,0 +1,126 @@
+import logging
+from pathlib import Path
+import time
+
+from ROAR_Sim.configurations.configuration import Configuration as CarlaConfig
+from ROAR_Sim.carla_client.carla_runner import CarlaRunner
+
+from ROAR.configurations.configuration import Configuration as AgentConfig
+from ROAR.agent_module.pure_pursuit_agent import PurePursuitAgent
+from ROAR.agent_module.special_agents.recording_agent import RecordingAgent
+from ROAR_Sim.carla_client.util.utilities import CarlaCarColor, CarlaCarColors
+
+# Agents
+from ROAR.agent_module.special_agents.waypoint_generating_agent import WaypointGeneratigAgent # agent for new waypoints generation
+from ROAR.agent_module.forward_only_agent import ForwardOnlyAgent 
+from ROAR.agent_module.pid_agent import PIDAgent
+from ROAR.agent_module.occu_map_demo_driving_agent import OccuMapDemoDrivingAgent # Occupancy Map Demo agent. Autonomous driving not supported yet.
+from ROAR.agent_module.occupancy_map_agent import OccupancyMapAgent
+from ROAR.agent_module.rl_pid_agent import RLPIDAgent # for rl agent training using Gym and Stable baseline.
+# from ROAR.agent_module.rl_testing_pid_agent import RLTestingAgent # rl pid agent demo, requires stable_baselines
+
+from pit_stop import PitStop as PitStop
+
+def main():
+    """Starts game loop"""
+    carla_config = CarlaConfig.parse_file(Path("./ROAR_Sim/configurations/configuration.json"))
+    agent_config = AgentConfig.parse_file(Path("./ROAR_Sim/configurations/agent_configuration.json"))
+
+    """
+    Pit Stop:
+        Use different kinds of 'set' functions at PitStop to tune/fix your own car!
+    """
+    pitstop = PitStop(carla_config, agent_config)
+    pitstop.set_carla_version(version = "0.9.10")
+    pitstop.set_carla_sync_mode(False)
+    pitstop.set_autopilot_mode(True)
+    pitstop.set_car_color(CarlaCarColor(r = 0,g = 0,b = 255,a = 255))
+    pitstop.set_num_laps(num=10)#1
+    pitstop.set_output_data_folder_path("./data/output")
+    pitstop.set_output_data_file_name(time.strftime("%Y%m%d-%H%M%S-") + "map-waypoints")
+    pitstop.set_max_speed(speed = 200)
+    pitstop.set_target_speed(speed = 190)
+    print(agent_config.target_speed, " target speed")
+    pitstop.set_steering_boundary(boundary = (-1.0, 1.0))
+    pitstop.set_throttle_boundary(boundary = (0, 1.0))
+    pitstop.set_waypoints_look_ahead_values(values={
+                                                    "60": 5,
+                                                    "80": 10,
+                                                    "120": 20,
+                                                    "150": 50,
+                                                    "180": 55})
+    pid_value = {
+                    "longitudinal_controller": {
+                        "40": {
+                            "Kp": 0.8,
+                            "Kd": 0.2,
+                            "Ki": 0
+                        },
+                        "60": {
+                            "Kp": 0.5,
+                            "Kd": 0.2,
+                            "Ki": 0
+                        },
+                        "150": {
+                            "Kp": 0.2,
+                            "Kd": 0.1,
+                            "Ki": 0.1
+                        },
+                        "180": {
+                            "Kp": 0.17,
+                            "Kd": 0.12,
+                            "Ki": 0.12
+                        }
+                    },
+                    "latitudinal_controller": {
+                        "60": {
+                            "Kp": 0.8,
+                            "Kd": 0.1,
+                            "Ki": 0.1
+                        },
+                        "100": {
+                            "Kp": 0.6,
+                            "Kd": 0.2,
+                            "Ki": 0.1
+                        },
+                        "150": {
+                            "Kp": 0.5,
+                            "Kd": 0.2,
+                            "Ki": 0.1
+                        },
+                        "180": {
+                            "Kp": 0.44,
+                            "Kd": 0.2,
+                            "Ki": 0.11
+                        }
+                    }
+    }
+    pitstop.set_pid_values(pid_value)
+
+    """Passing configurations to Carla and Agent"""
+    carla_runner = CarlaRunner(carla_settings=carla_config, # ROAR Academy: fine
+                               agent_settings=agent_config, # ROAR Academy: fine
+                               npc_agent_class=PurePursuitAgent)
+    try:
+        my_vehicle = carla_runner.set_carla_world()
+
+        agent = PIDAgent(vehicle=my_vehicle, agent_settings=agent_config)
+        # agent = WaypointGeneratigAgent(vehicle=my_vehicle, agent_settings=agent_config)
+        
+        carla_runner.start_game_loop(agent=agent, use_manual_control=False) # for PIDAgent
+        # carla_runner.start_game_loop(agent=agent, use_manual_control=True) # for WaypointGeneratingAgent
+    
+    except Exception as e:
+        logging.error(f"Something bad happened during initialization: {e}")
+        carla_runner.on_finish()
+        logging.error(f"{e}. Might be a good idea to restart Server")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(levelname)s - %(asctime)s - %(name)s '
+                               '- %(message)s',
+                        datefmt="%H:%M:%S",
+                        level=logging.DEBUG)
+    import warnings
+    warnings.filterwarnings("ignore", module="carla")
+    main()
